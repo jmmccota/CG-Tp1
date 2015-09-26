642b4c2132afffe209942c937add1aa1df514e3e
Elias Luiz <eliasluizjr@gmail.com>
2015-09-26 13:10:17 -0300

    Corrigido modelo bf109

--- CG-Tp1/Main.cpp
+++ CG-Tp1/Main.cpp
@@ -2,6 +2,7 @@
 #include "Solido.h"
 
 Solido *s = new Solido();
+Solido *s2 = new Solido();
 
 void Desenha(){
 
@@ -9,12 +10,15 @@ void Desenha(){
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glColor3f(1.0f, 0.0f, 0.0f);
-    gluOrtho2D(-1000, 1000, -1000, 1000);
+    gluOrtho2D(-2000, 2000, -2000, 2000);
 
     
     s->posX = 0;
-    s->posY = 0;
+    s->posY = -1000;
     s->desenha();
+    s2->posX = 0;
+    s2->posY = 1000;
+    s2->desenha();
 
     glFlush();
 }
@@ -22,7 +26,8 @@ void Desenha(){
 int main(int argc, char **argv) {
     char* titulo = "Animação";
 
-	s->carrega("spitfire.txt");
+    s->carrega("spitfire.txt");
+    s2->carrega("bf109.txt");
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(800, 600);
