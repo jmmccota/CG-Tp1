acb5fdc5f7eb1f8a41fa98e7a74d41545f439f51
jmmccota <joao_marcos_cota@hotmail.com>
2015-09-25 21:42:26 -0300

    Testes

--- CG-Tp1/Main.cpp
+++ CG-Tp1/Main.cpp
@@ -1,30 +1,14 @@
-//Ciclo principal do jogo (fase.terminou redireciona para ca
-//    se a fase tiver acabado (chamada dentro de fase.atualiza))
-//switch (faseAtual)
-//{
-//case 1:
-//	fase1.glSetup();
-//case 2:
-//	fase2.glSetup();
-//...
-//default:
-//	break;
-//}
-
-#ifdef _WIN32
-#include <Windows.h>
-#endif
-#include <gl/GL.h>
 #include <gl/glut.h>
 #include "Solido.h"
+
+Solido *s = new Solido();
 
 void Desenha(){
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    Solido *s = new Solido();
-    s->carrega("spitfire.txt");
+    
     s->posX = 0;
     s->posY = 0;
     s->desenha();
@@ -34,6 +18,8 @@ void Desenha(){
 
 int main(int argc, char **argv) {
     char* titulo = "Animação";
+
+	s->carrega("spitfire.txt");
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(800, 600);
