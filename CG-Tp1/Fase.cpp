a8b1907de554c507c59d88dd9f4fcb29fedbfdcf
daniel22390 <daniel22390@hotmail.com>
2015-10-12 17:42:44 -0300

    iniciando fundo

--- CG-Tp1/Fase.cpp
+++ CG-Tp1/Fase.cpp
@@ -10,6 +10,23 @@ Fase::~Fase()
     
 }
 
+void desenha2(){
+	glClearColor(0, 0.5, 1, 1);
+	glPushMatrix();
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-100, 100, -100, 100);
+	glPopMatrix();
+}
+
+void Fase::desenhaBackground(){
+	desenha2();
+}
+
+void Fase::desenha(){
+	desenhaBackground();
+}
+
 void Fase::novoProjetil(Projetil *p)
 {
     projeteis.push_back(p);
