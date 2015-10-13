9cc8c58979f5fe2322afad023cb8923182d69e3b
André Malta <andrmalta@gmail.com>
2015-10-13 14:13:14 -0300

    merge

--- CG-Tp1/Menu.cpp
+++ CG-Tp1/Menu.cpp
@@ -627,6 +627,6 @@ void Menu::inicializa()
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	//-------------
-	//EfeitoSonoro::getInstance().playMainTheme();
+	EfeitoSonoro::getInstance().playMainTheme();
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 }
