226f708419aee621deb2649fa85288add33f808d
daniel22390 <daniel22390@hotmail.com>
2015-10-19 23:14:51 -0200

    teste

---
+++ CG-TP/CG-TP/FMOD_Audio.cpp
@@ -0,0 +1,72 @@
+#include "FMOD_Audio.hpp"
+
+
+FMOD_Audio::FMOD_Audio(FMOD::System *system, char *file, bool loop)
+{
+
+	system->createSound(file, FMOD_DEFAULT, 0, &sound);
+	if (loop) {
+		sound->setMode(FMOD_LOOP_NORMAL);
+	}
+
+	bool isPlaying;
+
+	system->playSound(FMOD_CHANNEL_FREE, sound, true, &channel);
+	system->update();
+}
+
+FMOD_Audio::FMOD_Audio()
+{
+}
+
+FMOD::Channel * FMOD_Audio::getChannel()
+{
+	return this->channel;
+}
+
+FMOD::Sound * FMOD_Audio::getSound()
+{
+	return this->sound;
+}
+
+void FMOD_Audio::playStreamAudio(FMOD::System *system, char *file, bool loop)
+{
+	system->createStream(file, FMOD_HARDWARE, 0, &sound);
+	system->playSound(FMOD_CHANNEL_FREE, sound, false, &channel);
+
+	if (loop) {
+		this->sound->setMode(FMOD_LOOP_NORMAL);
+	}
+
+}
+
+void FMOD_Audio::playMemoryAudio(FMOD::System *system)
+{
+	bool isPaused = false;
+	channel->getPaused(&isPaused);
+
+	if (!isPaused) {
+		system->playSound(FMOD_CHANNEL_REUSE, sound, false, &channel);
+	}
+	else {
+		channel->setPaused(false);
+	}
+}
+
+void FMOD_Audio::stopAudio()
+{
+	channel->setPaused(true);
+}
+
+void FMOD_Audio::setVolume(float volume)
+{
+	if (volume >= 0 && volume <= 100) {
+		channel->setVolume(volume / 100);
+	}
+}
+
+
+
+FMOD_Audio::~FMOD_Audio()
+{
+}
---
+++ CG-TP/CG-TP/FMOD_Audio.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <fmod.hpp>
+#include <fmod_errors.h>
+#include <iostream>
+#include <vector>
+
+class FMOD_Audio
+{
+protected:
+	FMOD::Sound *sound;
+	FMOD::Channel *channel;
+public:
+	FMOD_Audio(FMOD::System *system, char *file, bool loop);
+	FMOD_Audio();
+	~FMOD_Audio();
+
+	//Getters
+	FMOD::Channel *getChannel();
+	FMOD::Sound *getSound();
+
+	//Play Once
+	void playStreamAudio(FMOD::System *system, char *file, bool loop);
+	//Save in memory
+	void playMemoryAudio(FMOD::System *system);
+	//Stop som
+	void stopAudio();
+	//Set volume 0 to 100
+	void setVolume(float volume);
+
+};
+
