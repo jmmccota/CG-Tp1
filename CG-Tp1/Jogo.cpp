98d0da0069c393401bc8a69bd7b7104ac60f508f
Elias Luiz <eliasluizjr@gmail.com>
2015-10-19 22:30:35 -0200

    Adicionado som mg42

--- CG-Tp1/Jogo.cpp
+++ CG-Tp1/Jogo.cpp
@@ -73,23 +73,20 @@ void Jogo::keyDown(unsigned char key, int x, int y)
 void Jogo::keyUp(unsigned char key, int x, int y)
 {
 	switch (key)
-	{
-<<<<<<< HEAD
-=======
-	/*case 'f':
-		EfeitoVisual::getInstance().setFullScreen();
-		break;*/
-	case 'p':
-	case 'P':
-		Jogo::getInstance().pausado = !Jogo::getInstance().pausado;
-		break;
->>>>>>> d44b144c77b3cca711767c866678007f2cac674f
-	case 27: //Tecla ESC -> Sair do Jogo
-		exit(0);
-		break;
-	default:
-		Jogo::getInstance().fases[Jogo::getInstance().proxFase]->keyUp(key, x, y);
-		break;
+	    {
+	    case 'f':
+		    EfeitoVisual::getInstance().setFullScreen();
+		    break;
+	    case 'p':
+	    case 'P':
+		    Jogo::getInstance().pausado = !Jogo::getInstance().pausado;
+		    break;
+	    case 27: //Tecla ESC -> Sair do Jogo
+		    exit(0);
+		    break;
+	    default:
+		    Jogo::getInstance().fases[Jogo::getInstance().proxFase]->keyUp(key, x, y);
+		    break;
 	}
 
 }
